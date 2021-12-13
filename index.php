7cdea80acd0a571d7f98d37d8c683abef1b5e13c
3DG <72684473+3DG@users.noreply.github.com>
2021-12-13 16:47:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,56 @@
-<html>
-  <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@700&display=swap" rel="stylesheet"> 
-    <title>Velator+</title>
-    <style>
-      h1 {
-        font-size:60px;
-      }
-      * {
-        margin:0px;
-        padding:0px;
-        font-family: 'Source Sans Pro', sans-serif;
-        color:white;
-      }
-      body {
-        background-color:#2c1e3e;
-      }
-      #maintext {
-        align:middle;
-        margin: 50px 0px;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="maintext" align="center">
-      <h1>Welcome to Velator+!</h1>
-      <h2>On this site, you can share robot 64 levels!</h2>
-      <h2>Coming soon!</h2>
-      <p>Planned completion date: December 23, 2021</p>
-    </div>
-  </body>
-</html>
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@700&amp;display=swap" rel="stylesheet"> 
+	<title>Velator+</title>
+	<style id="root">
+		:root {
+			--scroll:0px;
+		}
+	</style>
+	<script>
+		document.addEventListener("scroll", function() {
+		document.getElementById("root").innerHTML = `
+			:root {
+				--scroll:`+Math.floor(window.scrollY/2)+`px;
+			}
+		`
+		})
+	</script>
+	<style>
+	    
+		@keyframes moveRight {
+			0% {background-position:0px var(--scroll);}
+			100% {background-position:120px var(--scroll);}
+		}
+		* {
+			margin:0px;
+			padding:0px;
+			font-family:'Source Sans Pro', sans-serif;
+			color:white;
+		}
+		body {
+			background-color:#003;
+		}
+		#top {
+			animation-name:moveRight;
+			animation-duration:3s;
+			animation-iteration-count:infinite;
+			animation-timing-function: linear;
+			padding: 17.75% 0px;
+			background-image:url(./checker.png);
+			background-size:120px;
+			font-size:35px;
+		}
+		#top h1 {
+			font-size:150px;
+		}
+	</style>
+</head>
+<body>
+	<div id="top" align="center">
+	<h1>Velator+</h1>
+	<p>Work in progress!</p>
+	<p>Completion goal: <b>December 23, 2021</b></p>
+	</div>
+</body>
