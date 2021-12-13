86e519fbc1a0b29918716726fd5812ebb4f6f07a
3DG <72684473+3DG@users.noreply.github.com>
2021-12-13 10:00:22 -0500

    cool

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<h1>Welcome to Velator+!</h1>
-<p>On this site, you can share robot 64 levels!</p>
-<p>WIP. Planned completion date: December 23, 2021</p>
+<html>
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@700&display=swap" rel="stylesheet"> 
+    <title>Velator+</title>
+    <style>
+      h1 {
+        font-size:60px;
+      }
+      * {
+        margin:0px;
+        padding:0px;
+        font-family: 'Source Sans Pro', sans-serif;
+        color:white;
+      }
+      body {
+        background-color:#2c1e3e;
+      }
+      #maintext {
+        align:middle;
+        margin: 50px 0px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="maintext" align="center">
+      <h1>Welcome to Velator+!</h1>
+      <h2>On this site, you can share robot 64 levels!</h2>
+      <h2>Coming soon!</h2>
+      <p>Planned completion date: December 23, 2021</p>
+    </div>
+  </body>
+</html>
